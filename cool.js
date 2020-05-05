2f8a7e24dfda26c44e88e7a6fb0200645ca0106c
etinaude <etinaude@gmail.com>
2020-05-05 20:53:06 +1200

    fixed code formatting

--- cool.js
+++ cool.js
@@ -1,18 +1,22 @@
 var slideIndex = [1, 1];
 /* Class the members of each slideshow group with different CSS classes */
-var slideId = ["mySlides1", "mySlides2"]
+var slideId = ["mySlides1", "mySlides2"];
 showSlides(1, 0);
 showSlides(1, 1);
 
 function plusSlides(n, no) {
-    showSlides(slideIndex[no] += n, no);
+    showSlides((slideIndex[no] += n), no);
 }
 
 function showSlides(n, no) {
     var i;
     var x = document.getElementsByClassName(slideId[no]);
-    if (n > x.length) { slideIndex[no] = 1 }
-    if (n < 1) { slideIndex[no] = x.length }
+    if (n > x.length) {
+        slideIndex[no] = 1;
+    }
+    if (n < 1) {
+        slideIndex[no] = x.length;
+    }
     for (i = 0; i < x.length; i++) {
         x[i].style.display = "none";
     }
