e93ba89958ce570c93887f72cc385c0a1b8c7fb8
etinaude <etinaude@gmail.com>
2020-05-05 20:25:04 +1200

    menu is extended by defualt

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 /* Set the width of the side navigation to 250px */
 function openNav() {
-    document.getElementById("mySidenav").style.width = "300px";
+    document.getElementById("mySidenav").style.width = "250px";
 }
 
 /* Set the width of the side navigation to 0 */
