69df7aa5f193142fa5e0cb4e21244a72f6798fb5
Etienne <etinaude@gmail.com>
2020-10-03 20:37:42 +1300

    defer css

---
+++ js/deferCSS.js
@@ -0,0 +1,19 @@
+var CSS = ["fonts/flaticon/font/flaticon.css", "fonts/icomoon/style.css"];
+
+var loadDeferredStyles = function () {
+  var addStylesNode = document.getElementById("deferred-styles");
+  var replacement = document.createElement("div");
+  replacement.innerHTML = addStylesNode.textContent;
+  document.body.appendChild(replacement);
+  addStylesNode.parentElement.removeChild(addStylesNode);
+};
+var raf =
+  window.requestAnimationFrame ||
+  window.mozRequestAnimationFrame ||
+  window.webkitRequestAnimationFrame ||
+  window.msRequestAnimationFrame;
+if (raf)
+  raf(function () {
+    window.setTimeout(loadDeferredStyles, 0);
+  });
+else window.addEventListener("load", loadDeferredStyles);
