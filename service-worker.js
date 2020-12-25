38c702adb774589225c72804aebeec1d9877763a
Etienne <etinaude@gmail.com>
2020-12-26 01:50:12 +1300

    info update

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 "use strict";
 
-var cacheVersion = 1;
+var cacheVersion = 2;
 var currentCache = {
   offline: "offline-cache" + cacheVersion,
 };
@@ -20,8 +20,6 @@ this.addEventListener("install", (event) => {
 });
 
 this.addEventListener("fetch", (event) => {
-  // request.mode = navigate isn't supported in all browsers
-  // so include a check for Accept: text/html header.
   if (
     event.request.mode === "navigate" ||
     (event.request.method === "GET" &&
@@ -29,12 +27,10 @@ this.addEventListener("fetch", (event) => {
   ) {
     event.respondWith(
       fetch(event.request.url).catch((error) => {
-        // Return the offline page
         return caches.match(offlineUrl);
       })
     );
   } else {
-    // Respond with everything else if we can
     event.respondWith(
       caches.match(event.request).then(function (response) {
         return response || fetch(event.request);
