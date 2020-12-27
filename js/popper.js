1011d4d5e5a33766df69ed008607e92bf624980e
Etienne <etinaude@gmail.com>
2020-12-28 00:23:10 +1300

    removed unused code

---
+++ js/popper.js
@@ -0,0 +1,375 @@
+/*
+ Copyright (C) Federico Zivolo 2018
+ Distributed under the MIT License (license terms are at http://opensource.org/licenses/MIT).
+ */
+(function (e, t) {
+    'object' == typeof exports && 'undefined' != typeof module ? module.exports = t() : 'function' == typeof define && define.amd ? define(t) : e.Popper = t()
+})(this, function () {
+    'use strict';
+
+    function X(e, t) {
+        var o, n = t.offset,
+            i = e.placement,
+            r = e.offsets,
+            p = r.popper,
+            s = r.reference,
+            d = i.split('-')[0];
+        return o = Y(+n) ? [+n, 0] : _(n, p, s, d), 'left' === d ? (p.top += o[0], p.left -= o[1]) : 'right' === d ? (p.top += o[0], p.left += o[1]) : 'top' === d ? (p.left += o[0], p.top -= o[1]) : 'bottom' === d && (p.left += o[0], p.top += o[1]), e.popper = p, e
+    }
+    for (var J = Math.min, Q = Math.round, Z = Math.floor, $ = Math.max, ee = 'undefined' != typeof window && 'undefined' != typeof document, te = ['Edge', 'Trident', 'Firefox'], oe = 0, ne = 0; ne < te.length; ne += 1)
+        if (ee && 0 <= navigator.userAgent.indexOf(te[ne])) {
+            oe = 1;
+            break
+        } var i = ee && window.Promise,
+            ie = i ? function (e) {
+                var t = !1;
+                return function () {
+                    t || (t = !0, window.Promise.resolve().then(function () {
+                        t = !1, e()
+                    }))
+                }
+            } : function (e) {
+                var t = !1;
+                return function () {
+                    t || (t = !0, setTimeout(function () {
+                        t = !1, e()
+                    }, oe))
+                }
+            },
+            re = ee && !!(window.MSInputMethodContext && document.documentMode),
+            pe = ee && /MSIE 10/.test(navigator.userAgent),
+            se = function (e, t) {
+                if (!(e instanceof t)) throw new TypeError('Cannot call a class as a function')
+            },
+            de = function () {
+                function e(e, t) {
+                    for (var o, n = 0; n < t.length; n++) o = t[n], o.enumerable = o.enumerable || !1, o.configurable = !0, 'value' in o && (o.writable = !0), Object.defineProperty(e, o.key, o)
+                }
+                return function (t, o, n) {
+                    return o && e(t.prototype, o), n && e(t, n), t
+                }
+            }(),
+            ae = function (e, t, o) {
+                return t in e ? Object.defineProperty(e, t, {
+                    value: o,
+                    enumerable: !0,
+                    configurable: !0,
+                    writable: !0
+                }) : e[t] = o, e
+            },
+            le = Object.assign || function (e) {
+                for (var t, o = 1; o < arguments.length; o++)
+                    for (var n in t = arguments[o], t) Object.prototype.hasOwnProperty.call(t, n) && (e[n] = t[n]);
+                return e
+            },
+            fe = ['auto-start', 'auto', 'auto-end', 'top-start', 'top', 'top-end', 'right-start', 'right', 'right-end', 'bottom-end', 'bottom', 'bottom-start', 'left-end', 'left', 'left-start'],
+            me = fe.slice(3),
+            he = {
+                FLIP: 'flip',
+                CLOCKWISE: 'clockwise',
+                COUNTERCLOCKWISE: 'counterclockwise'
+            },
+            ce = function () {
+                function t(o, n) {
+                    var i = this,
+                        r = 2 < arguments.length && void 0 !== arguments[2] ? arguments[2] : {};
+                    se(this, t), this.scheduleUpdate = function () {
+                        return requestAnimationFrame(i.update)
+                    }, this.update = ie(this.update.bind(this)), this.options = le({}, t.Defaults, r), this.state = {
+                        isDestroyed: !1,
+                        isCreated: !1,
+                        scrollParents: []
+                    }, this.reference = o && o.jquery ? o[0] : o, this.popper = n && n.jquery ? n[0] : n, this.options.modifiers = {}, Object.keys(le({}, t.Defaults.modifiers, r.modifiers)).forEach(function (e) {
+                        i.options.modifiers[e] = le({}, t.Defaults.modifiers[e] || {}, r.modifiers ? r.modifiers[e] : {})
+                    }), this.modifiers = Object.keys(this.options.modifiers).map(function (e) {
+                        return le({
+                            name: e
+                        }, i.options.modifiers[e])
+                    }).sort(function (e, t) {
+                        return e.order - t.order
+                    }), this.modifiers.forEach(function (t) {
+                        t.enabled && e(t.onLoad) && t.onLoad(i.reference, i.popper, i.options, t, i.state)
+                    }), this.update();
+                    var p = this.options.eventsEnabled;
+                    p && this.enableEventListeners(), this.state.eventsEnabled = p
+                }
+                return de(t, [{
+                    key: 'update',
+                    value: function () {
+                        return k.call(this)
+                    }
+                }, {
+                    key: 'destroy',
+                    value: function () {
+                        return H.call(this)
+                    }
+                }, {
+                    key: 'enableEventListeners',
+                    value: function () {
+                        return F.call(this)
+                    }
+                }, {
+                    key: 'disableEventListeners',
+                    value: function () {
+                        return U.call(this)
+                    }
+                }]), t
+            }();
+    return ce.Utils = ('undefined' == typeof window ? global : window).PopperUtils, ce.placements = fe, ce.Defaults = {
+        placement: 'bottom',
+        positionFixed: !1,
+        eventsEnabled: !0,
+        removeOnDestroy: !1,
+        onCreate: function () { },
+        onUpdate: function () { },
+        modifiers: {
+            shift: {
+                order: 100,
+                enabled: !0,
+                fn: function (e) {
+                    var t = e.placement,
+                        o = t.split('-')[0],
+                        n = t.split('-')[1];
+                    if (n) {
+                        var i = e.offsets,
+                            r = i.reference,
+                            p = i.popper,
+                            s = -1 !== ['bottom', 'top'].indexOf(o),
+                            d = s ? 'left' : 'top',
+                            a = s ? 'width' : 'height',
+                            l = {
+                                start: ae({}, d, r[d]),
+                                end: ae({}, d, r[d] + r[a] - p[a])
+                            };
+                        e.offsets.popper = le({}, p, l[n])
+                    }
+                    return e
+                }
+            },
+            offset: {
+                order: 200,
+                enabled: !0,
+                fn: X,
+                offset: 0
+            },
+            preventOverflow: {
+                order: 300,
+                enabled: !0,
+                fn: function (e, t) {
+                    var o = t.boundariesElement || p(e.instance.popper);
+                    e.instance.reference === o && (o = p(o));
+                    var n = B('transform'),
+                        i = e.instance.popper.style,
+                        r = i.top,
+                        s = i.left,
+                        d = i[n];
+                    i.top = '', i.left = '', i[n] = '';
+                    var a = v(e.instance.popper, e.instance.reference, t.padding, o, e.positionFixed);
+                    i.top = r, i.left = s, i[n] = d, t.boundaries = a;
+                    var l = t.priority,
+                        f = e.offsets.popper,
+                        m = {
+                            primary: function (e) {
+                                var o = f[e];
+                                return f[e] < a[e] && !t.escapeWithReference && (o = $(f[e], a[e])), ae({}, e, o)
+                            },
+                            secondary: function (e) {
+                                var o = 'right' === e ? 'left' : 'top',
+                                    n = f[o];
+                                return f[e] > a[e] && !t.escapeWithReference && (n = J(f[o], a[e] - ('right' === e ? f.width : f.height))), ae({}, o, n)
+                            }
+                        };
+                    return l.forEach(function (e) {
+                        var t = -1 === ['left', 'top'].indexOf(e) ? 'secondary' : 'primary';
+                        f = le({}, f, m[t](e))
+                    }), e.offsets.popper = f, e
+                },
+                priority: ['left', 'right', 'top', 'bottom'],
+                padding: 5,
+                boundariesElement: 'scrollParent'
+            },
+            keepTogether: {
+                order: 400,
+                enabled: !0,
+                fn: function (e) {
+                    var t = e.offsets,
+                        o = t.popper,
+                        n = t.reference,
+                        i = e.placement.split('-')[0],
+                        r = Z,
+                        p = -1 !== ['top', 'bottom'].indexOf(i),
+                        s = p ? 'right' : 'bottom',
+                        d = p ? 'left' : 'top',
+                        a = p ? 'width' : 'height';
+                    return o[s] < r(n[d]) && (e.offsets.popper[d] = r(n[d]) - o[a]), o[d] > r(n[s]) && (e.offsets.popper[d] = r(n[s])), e
+                }
+            },
+            arrow: {
+                order: 500,
+                enabled: !0,
+                fn: function (e, o) {
+                    var n;
+                    if (!q(e.instance.modifiers, 'arrow', 'keepTogether')) return e;
+                    var i = o.element;
+                    if ('string' == typeof i) {
+                        if (i = e.instance.popper.querySelector(i), !i) return e;
+                    } else if (!e.instance.popper.contains(i)) return console.warn('WARNING: `arrow.element` must be child of its popper element!'), e;
+                    var r = e.placement.split('-')[0],
+                        p = e.offsets,
+                        s = p.popper,
+                        d = p.reference,
+                        a = -1 !== ['left', 'right'].indexOf(r),
+                        l = a ? 'height' : 'width',
+                        f = a ? 'Top' : 'Left',
+                        m = f.toLowerCase(),
+                        h = a ? 'left' : 'top',
+                        c = a ? 'bottom' : 'right',
+                        u = S(i)[l];
+                    d[c] - u < s[m] && (e.offsets.popper[m] -= s[m] - (d[c] - u)), d[m] + u > s[c] && (e.offsets.popper[m] += d[m] + u - s[c]), e.offsets.popper = g(e.offsets.popper);
+                    var b = d[m] + d[l] / 2 - u / 2,
+                        y = t(e.instance.popper),
+                        w = parseFloat(y['margin' + f], 10),
+                        E = parseFloat(y['border' + f + 'Width'], 10),
+                        v = b - e.offsets.popper[m] - w - E;
+                    return v = $(J(s[l] - u, v), 0), e.arrowElement = i, e.offsets.arrow = (n = {}, ae(n, m, Q(v)), ae(n, h, ''), n), e
+                },
+                element: '[x-arrow]'
+            },
+            flip: {
+                order: 600,
+                enabled: !0,
+                fn: function (e, t) {
+                    if (W(e.instance.modifiers, 'inner')) return e;
+                    if (e.flipped && e.placement === e.originalPlacement) return e;
+                    var o = v(e.instance.popper, e.instance.reference, t.padding, t.boundariesElement, e.positionFixed),
+                        n = e.placement.split('-')[0],
+                        i = T(n),
+                        r = e.placement.split('-')[1] || '',
+                        p = [];
+                    switch (t.behavior) {
+                        case he.FLIP:
+                            p = [n, i];
+                            break;
+                        case he.CLOCKWISE:
+                            p = z(n);
+                            break;
+                        case he.COUNTERCLOCKWISE:
+                            p = z(n, !0);
+                            break;
+                        default:
+                            p = t.behavior;
+                    }
+                    return p.forEach(function (s, d) {
+                        if (n !== s || p.length === d + 1) return e;
+                        n = e.placement.split('-')[0], i = T(n);
+                        var a = e.offsets.popper,
+                            l = e.offsets.reference,
+                            f = Z,
+                            m = 'left' === n && f(a.right) > f(l.left) || 'right' === n && f(a.left) < f(l.right) || 'top' === n && f(a.bottom) > f(l.top) || 'bottom' === n && f(a.top) < f(l.bottom),
+                            h = f(a.left) < f(o.left),
+                            c = f(a.right) > f(o.right),
+                            g = f(a.top) < f(o.top),
+                            u = f(a.bottom) > f(o.bottom),
+                            b = 'left' === n && h || 'right' === n && c || 'top' === n && g || 'bottom' === n && u,
+                            y = -1 !== ['top', 'bottom'].indexOf(n),
+                            w = !!t.flipVariations && (y && 'start' === r && h || y && 'end' === r && c || !y && 'start' === r && g || !y && 'end' === r && u);
+                        (m || b || w) && (e.flipped = !0, (m || b) && (n = p[d + 1]), w && (r = G(r)), e.placement = n + (r ? '-' + r : ''), e.offsets.popper = le({}, e.offsets.popper, C(e.instance.popper, e.offsets.reference, e.placement)), e = P(e.instance.modifiers, e, 'flip'))
+                    }), e
+                },
+                behavior: 'flip',
+                padding: 5,
+                boundariesElement: 'viewport'
+            },
+            inner: {
+                order: 700,
+                enabled: !1,
+                fn: function (e) {
+                    var t = e.placement,
+                        o = t.split('-')[0],
+                        n = e.offsets,
+                        i = n.popper,
+                        r = n.reference,
+                        p = -1 !== ['left', 'right'].indexOf(o),
+                        s = -1 === ['top', 'left'].indexOf(o);
+                    return i[p ? 'left' : 'top'] = r[o] - (s ? i[p ? 'width' : 'height'] : 0), e.placement = T(t), e.offsets.popper = g(i), e
+                }
+            },
+            hide: {
+                order: 800,
+                enabled: !0,
+                fn: function (e) {
+                    if (!q(e.instance.modifiers, 'hide', 'preventOverflow')) return e;
+                    var t = e.offsets.reference,
+                        o = D(e.instance.modifiers, function (e) {
+                            return 'preventOverflow' === e.name
+                        }).boundaries;
+                    if (t.bottom < o.top || t.left > o.right || t.top > o.bottom || t.right < o.left) {
+                        if (!0 === e.hide) return e;
+                        e.hide = !0, e.attributes['x-out-of-boundaries'] = ''
+                    } else {
+                        if (!1 === e.hide) return e;
+                        e.hide = !1, e.attributes['x-out-of-boundaries'] = !1
+                    }
+                    return e
+                }
+            },
+            computeStyle: {
+                order: 850,
+                enabled: !0,
+                fn: function (e, t) {
+                    var o = t.x,
+                        n = t.y,
+                        i = e.offsets.popper,
+                        r = D(e.instance.modifiers, function (e) {
+                            return 'applyStyle' === e.name
+                        }).gpuAcceleration;
+                    void 0 !== r && console.warn('WARNING: `gpuAcceleration` option moved to `computeStyle` modifier and will not be supported in future versions of Popper.js!');
+                    var s, d, a = void 0 === r ? t.gpuAcceleration : r,
+                        l = p(e.instance.popper),
+                        f = u(l),
+                        m = {
+                            position: i.position
+                        },
+                        h = {
+                            left: Z(i.left),
+                            top: Q(i.top),
+                            bottom: Q(i.bottom),
+                            right: Z(i.right)
+                        },
+                        c = 'bottom' === o ? 'top' : 'bottom',
+                        g = 'right' === n ? 'left' : 'right',
+                        b = B('transform');
+                    if (d = 'bottom' == c ? -f.height + h.bottom : h.top, s = 'right' == g ? -f.width + h.right : h.left, a && b) m[b] = 'translate3d(' + s + 'px, ' + d + 'px, 0)', m[c] = 0, m[g] = 0, m.willChange = 'transform';
+                    else {
+                        var y = 'bottom' == c ? -1 : 1,
+                            w = 'right' == g ? -1 : 1;
+                        m[c] = d * y, m[g] = s * w, m.willChange = c + ', ' + g
+                    }
+                    var E = {
+                        "x-placement": e.placement
+                    };
+                    return e.attributes = le({}, E, e.attributes), e.styles = le({}, m, e.styles), e.arrowStyles = le({}, e.offsets.arrow, e.arrowStyles), e
+                },
+                gpuAcceleration: !0,
+                x: 'bottom',
+                y: 'right'
+            },
+            applyStyle: {
+                order: 900,
+                enabled: !0,
+                fn: function (e) {
+                    return j(e.instance.popper, e.styles), K(e.instance.popper, e.attributes), e.arrowElement && Object.keys(e.arrowStyles).length && j(e.arrowElement, e.arrowStyles), e
+                },
+                onLoad: function (e, t, o, n, i) {
+                    var r = L(i, t, e, o.positionFixed),
+                        p = O(o.placement, r, t, e, o.modifiers.flip.boundariesElement, o.modifiers.flip.padding);
+                    return t.setAttribute('x-placement', p), j(t, {
+                        position: o.positionFixed ? 'fixed' : 'absolute'
+                    }), o
+                },
+                gpuAcceleration: void 0
+            }
+        }
+    }, ce
+});
+//# sourceMappingURL=popper.min.js.map
