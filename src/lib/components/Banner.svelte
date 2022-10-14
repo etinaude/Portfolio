b107fd91f905e381b43e4ce0cb4ba3e38fff4f68
Etienne <eti@naude.dev>
2022-10-14 16:18:11 +1300

    banner

---
+++ src/lib/components/Banner.svelte
@@ -0,0 +1,68 @@
+<script>
+  export let bannerInfo = {
+    style: "",
+    text: "",
+    display: false,
+  };
+</script>
+
+<div
+  class="banner {bannerInfo.style}  {bannerInfo.display ? 'shown' : 'hidden'}"
+>
+  <div class="text">
+    {bannerInfo.text}
+  </div>
+</div>
+
+<style lang="scss">
+  .banner {
+    border-radius: 10px;
+    text-align: center;
+    font-weight: bold;
+
+    position: fixed;
+    top: 10px;
+    left: 50%;
+    transform: translate(-50%, 0);
+    overflow: hidden;
+    z-index: 10000;
+    transition: 0.5s ease;
+
+    display: flex;
+    align-content: center;
+    justify-content: center;
+
+    &.hidden {
+      width: 0px;
+      height: 0px;
+      padding: 0px;
+    }
+
+    &.shown {
+      width: 90%;
+      height: auto;
+      padding: 1rem;
+    }
+  }
+
+  .text {
+    min-width: 60vh;
+    text-align: center;
+  }
+
+  .success {
+    background-color: #43ff82;
+
+    .text {
+      color: #222;
+    }
+  }
+
+  .error {
+    background-color: #ff435c;
+  }
+
+  .loading {
+    background-color: #43d0ff;
+  }
+</style>
