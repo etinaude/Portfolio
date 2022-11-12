4c31f2bad9598d66cab65992cf6ae36eae632531
Etienne <eti@naude.dev>
2022-11-12 17:49:14 +1300

    basic cursor

---
+++ src/lib/components/cursor.svelte
@@ -0,0 +1,49 @@
+<script context="module" lang="ts">
+  export const prerender = false;
+</script>
+
+<script lang="ts">
+  export let pos: {
+    x: number;
+    y: number;
+  };
+
+  const outerOffest = 12;
+  const innerOffest = 3;
+</script>
+
+<div
+  class="outer"
+  style="top: {pos.y - outerOffest}px; left: {pos.x - outerOffest}px;"
+>
+  <div
+    class="inner"
+    style="top: {pos.y - innerOffest}px; left: {pos.x - innerOffest}px;"
+  />
+</div>
+
+<style lang="scss">
+  .outer,
+  .inner {
+    border-radius: 50%;
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: 1000;
+    pointer-events: none;
+    transition: none;
+  }
+
+  .outer {
+    width: 20px;
+    height: 20px;
+    background-color: transparent;
+    border: 1px solid white;
+  }
+
+  .inner {
+    width: 4px;
+    height: 4px;
+    background-color: #43d0ff;
+  }
+</style>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,10 +1,21 @@
 <script lang="ts">
   import Header from "$lib/components/header.svelte";
+  import Cursor from "$lib/components/cursor.svelte";
+
   import "../app.scss";
+
+  let pos = { x: 0, y: 0 };
+
+  function mouseMove(event: any) {
+    pos.x = event.x;
+    pos.y = event.y;
+  }
 </script>
 
 <Header />
 
-<main>
+<main on:mousemove={mouseMove}>
   <slot />
+
+  <Cursor {pos} />
 </main>
