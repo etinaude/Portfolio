f89aa4b0b63354a5b36c0b857ebaf6dbf5de163a
Etienne <eti@naude.dev>
2022-11-20 15:18:35 +1300

    run webserver for tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -53,10 +53,10 @@ const config: PlaywrightTestConfig = {
   outputDir: "tests/results/",
 
   /* Run your local dev server before starting the tests */
-  //   webServer: {
-  //     command: "npm run dev",
-  //     port: 3000,
-  //   },
+  webServer: {
+    command: "npm run dev",
+    port: 3000,
+  },
 };
 
 export default config;
