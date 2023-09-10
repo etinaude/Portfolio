6d6685f549c96fd53024a7154a681b1d91346d2d
Etienne <eti@naude.dev>
2023-09-11 00:31:16 +0100

    fix bun migration bugs

--- new/portfolio/src/lib/components/Cursor.svelte
+++ new/portfolio/src/lib/components/Cursor.svelte
@@ -1,83 +1,83 @@
 <script lang="ts">
-  import type { posT } from "$lib/types/types";
+	import type { posT } from '$lib/types/types';
 
-  export let pos: posT;
+	export let pos: posT;
 
-  const outerOffest = 12;
+	const outerOffest = 12;
 </script>
 
 <div
-  class="outer {pos.clickable ? 'clickable' : ''}"
-  style="translate:  {pos.x - outerOffest}px {pos.y - outerOffest}px"
+	class="outer {pos.clickable ? 'clickable' : ''}"
+	style="translate:  {pos.x - outerOffest}px {pos.y - outerOffest}px"
 >
-  <div class="inner" />
+	<div class="inner" />
 </div>
 
 <style lang="scss">
-  @import "../styles/root.scss";
+	@import '../styles/root.scss';
 
-  .outer {
-    @include flex-center;
+	.outer {
+		@include flex-center;
 
-    border-radius: 50%;
-    position: fixed;
-    top: 0;
-    left: 0;
-    z-index: 1000;
-    pointer-events: none;
-    transition: none;
-    width: 20px;
-    height: 20px;
-    background-color: transparent;
-    border: 1px solid $light;
+		border-radius: 50%;
+		position: fixed;
+		top: 0;
+		left: 0;
+		z-index: 1000;
+		pointer-events: none;
+		transition: none;
+		width: 20px;
+		height: 20px;
+		background-color: transparent;
+		border: 1px solid $light;
 
-    &.clickable {
-      cursor: pointer;
-      animation: pulse 1s infinite;
+		&.clickable {
+			cursor: pointer;
+			animation: pulse 1s infinite;
 
-      .inner {
-        background-color: $light;
-        width: 5px;
-        height: 5px;
-        border-radius: 50%;
-      }
-    }
-  }
+			.inner {
+				background-color: $light;
+				width: 5px;
+				height: 5px;
+				border-radius: 50%;
+			}
+		}
+	}
 
-  @keyframes pulse {
-    0% {
-      scale: 1;
-      border-color: $light;
-    }
-    100% {
-      scale: 1.5;
-      border-color: $accent;
-    }
-  }
+	@keyframes pulse {
+		0% {
+			scale: 1;
+			border-color: $light;
+		}
+		100% {
+			scale: 1.5;
+			border-color: $accent;
+		}
+	}
 
-  .inner {
-    width: 4px;
-    height: 4px;
-    background-color: $accent;
-    border-radius: 50%;
-  }
+	.inner {
+		width: 4px;
+		height: 4px;
+		background-color: $accent;
+		border-radius: 50%;
+	}
 
-  @media (prefers-color-scheme: dark) {
-    .outer {
-      border: 1px solid $light;
-    }
-  }
+	@media (prefers-color-scheme: dark) {
+		.outer {
+			border: 1px solid $light;
+		}
+	}
 
-  @media (hover: none) {
-    .outer,
-    .inner {
-      display: none;
-    }
-  }
-  @media (pointer: coarse) {
-    .outer,
-    .inner {
-      display: none;
-    }
-  }
+	@media (hover: none) {
+		.outer,
+		.inner {
+			display: none;
+		}
+	}
+	@media (pointer: coarse) {
+		.outer,
+		.inner {
+			display: none;
+		}
+	}
 </style>
---
+++ new/portfolio/src/routes/+layout.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import Header from '$lib/components/Header.svelte';
+	import Cursor from '$lib/components/Cursor.svelte';
+	import Splash from '$lib/components/Splash.svelte';
+
+	import './app.scss';
+	import type { posT } from '$lib/types/types';
+
+	let pos: posT = { x: 0, y: 0, clickable: false };
+
+	function mouseMove(event: any) {
+		pos.x = event.x;
+		pos.y = event.y;
+
+		for (const el of event.composedPath()) {
+			if (el.classList?.contains('clickable')) {
+				pos.clickable = true;
+				return;
+			}
+		}
+		pos.clickable = false;
+	}
+</script>
+
+<main on:mousemove={mouseMove}>
+	<Header />
+	<slot />
+
+	<Cursor {pos} />
+	<Splash />
+</main>
