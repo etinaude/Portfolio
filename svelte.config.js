79f0fd7187bf1be5da3d935bfa6b78cee68f0a82
Etienne <eti@naude.dev>
2023-09-11 10:37:33 +0100

    vite optimisation

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,28 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-netlify';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+import { ViteMinifyPlugin } from 'vite-plugin-minify'
+import { optimizeCssModules } from 'vite-plugin-optimize-css-modules';
+import { VitePWA } from 'vite-plugin-pwa'
+
+
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
-	}
+		adapter: adapter({
+			edge: true,
+			split: false
+		}),
+
+	},
+
+	plugins: [
+		ViteMinifyPlugin({}),
+		optimizeCssModules(),
+		VitePWA()
+	],
 };
 
 export default config;
