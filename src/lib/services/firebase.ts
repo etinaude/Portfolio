5727d719029b1987f914d53416b5a62f8c3fa66b
Etienne <eti@naude.dev>
2023-09-13 12:36:45 +0100

    firebase service

---
+++ src/lib/services/firebase.ts
@@ -0,0 +1,91 @@
+import { initializeApp } from 'firebase/app';
+import KEYS from '../../keys.json';
+import { getFirestore, collection, query, getDocs, setDoc, doc } from 'firebase/firestore';
+
+const BASE_PATH = 'portfolio/all-data/';
+// Initialize Firebase
+const app = initializeApp(KEYS.firebase);
+const db = getFirestore(app);
+
+export function getAwardData() {
+	return getData(BASE_PATH + 'awards');
+}
+
+export function getSocialData() {
+	// setData();
+	return getData(BASE_PATH + 'socials');
+}
+
+export function getClubsData() {
+	return getData(BASE_PATH + 'clubs');
+}
+
+export function getEducationData() {
+	return getData(BASE_PATH + 'education');
+}
+
+export function getProjectsData() {
+	return getData(BASE_PATH + 'projects');
+}
+
+export function getWorkData() {
+	return getData(BASE_PATH + 'work');
+}
+
+export async function getData(collectionPath: string) {
+	try {
+		const db = getFirestore(app);
+		const q = query(collection(db, collectionPath));
+		const querySnapshot = await getDocs(q);
+		const data: any = [];
+		querySnapshot.forEach((doc) => {
+			data.push(doc.data());
+		});
+		return data;
+	} catch (e) {
+		console.error(e);
+	}
+}
+
+export async function getImage(collectionPath: string) {
+	try {
+		const db = getFirestore(app);
+		const q = query(collection(db, collectionPath));
+		const querySnapshot = await getDocs(q);
+		const data: any = [];
+		querySnapshot.forEach((doc) => {
+			data.push(doc.data());
+		});
+		return data;
+	} catch (e) {
+		console.error(e);
+	}
+}
+
+export async function setData() {
+	const linksImport: any = []; // rplace with data to upload
+	const uploadCollection = 'projects';
+	linksImport.forEach(async (link: any) => {
+		const docInfo: any = {
+			title: link.title,
+			description: link.description,
+			imageUrl: `https://firebasestorage.googleapis.com/v0/b/portfolio-aa70a.appspot.com/o/projects%2F${link.image_url}?alt=media`
+		};
+		if (link.hover_img) {
+			docInfo[
+				'hoverImg'
+			] = `https://firebasestorage.googleapis.com/v0/b/portfolio-aa70a.appspot.com/o/projects%2F${link.hover_img}?alt=media`;
+		}
+		if (link.hover_video) {
+			docInfo[
+				'hoverVideo'
+			] = `https://firebasestorage.googleapis.com/v0/b/portfolio-aa70a.appspot.com/o/projects%2F${link.hover_video}?alt=media`;
+		}
+		if (link.follow_url) {
+			docInfo['followUrl'] = link.follow_url;
+		}
+		const name = link.title.trim().replace(' ', '');
+		console.log(docInfo);
+		await setDoc(doc(db, BASE_PATH + uploadCollection + '/' + name), docInfo);
+	});
+}
