a35ebdf7dfd00f4e440ac739e487e424cd8b2e47
Etienne Naude <eti@naude.dev>
2024-08-26 02:04:15 +0100

    contacts simplified

--- src/lib/components/contact/Contact.svelte
+++ src/lib/components/contact/Contact.svelte
@@ -1,70 +1,20 @@
 <script lang="ts">
 	import Banner from '$lib/components/Banner.svelte';
-	import MailGun from '$lib/services/mailgunService';
-	import { ContactFormT } from '$lib/types/types';
 	import Saos from 'saos';
 	import Links from '$lib/components/contact/LinksComp.svelte';
 	import MiniProfile from '$lib/components/contact/MiniProfile.svelte';
 
-	let form: ContactFormT = new ContactFormT();
 	let banner: Banner;
-
-	async function sendEmail() {
-		const validate = MailGun.validateAll(form);
-
-		banner.show('Sending', 'info');
-
-		if (validate.valid) {
-			await MailGun.sendEmail(form);
-			banner.show('Sent! I look forward to talking with you.', 'success');
-
-			form = {
-				name: '',
-				email: '',
-				message: ''
-			};
-		} else {
-			banner.show(validate.message, 'error');
-		}
-	}
 </script>
 
 <div class="contacts">
-	<div class="form" id="form">
+	<div class="profile">
 		<Saos animation={'from-left 1s ease'}>
 			<MiniProfile />
-			<div class="row">
-				<input
-					type="text"
-					name="name"
-					placeholder="name"
-					bind:value={form.name}
-					class="clickable"
-				/>
-				<input
-					type="email"
-					name="email"
-					placeholder="email"
-					bind:value={form.email}
-					class="clickable"
-				/>
-			</div>
-
-			<textarea name="message" placeholder="message" bind:value={form.message} class="clickable" />
-			<button on:click={sendEmail} class="clickable"
-				>Send
-				<span class="material-symbol"> send </span>
-			</button>
 		</Saos>
 	</div>
 
-	<div>
-		<div class="mobile-profile">
-			<MiniProfile />
-		</div>
-
-		<Links />
-	</div>
+	<Links />
 </div>
 
 <Banner bind:this={banner} />
@@ -79,63 +29,8 @@
 		display: flex;
 		flex-direction: row;
 		justify-content: space-around;
-		align-items: flex-end;
+		align-items: center;
 		padding: 0 10vw;
-	}
-
-	.form {
-		width: 500px;
-
-		input,
-		textarea {
-			@include border-d;
-			background-color: $primary-xxl;
-			padding: 20px;
-			box-sizing: border-box;
-			font-size: max(0.7vw, 20px);
-			color: $light;
-
-			&:focus {
-				outline: 2px solid $accent;
-				scale: 1.05;
-			}
-
-			&:hover {
-				scale: 1.05;
-			}
-		}
-
-		.row {
-			display: flex;
-			flex-direction: row;
-			justify-content: space-between;
-			margin-bottom: 0px;
-		}
-
-		textarea {
-			resize: none;
-			height: 200px;
-			width: 100%;
-			margin-top: 0px;
-		}
-
-		button {
-			@include button;
-
-			width: 100%;
-			margin: 0;
-			font-size: 1rem;
-			margin-top: 10px;
-		}
-	}
-
-	input {
-		width: calc(50% - 10px);
-		margin-bottom: 20px;
-
-		&:first-child {
-			margin-right: 20px;
-		}
 	}
 
 	.mobile-profile {
@@ -144,20 +39,20 @@
 	}
 
 	@media (max-width: 1024px) {
-		.contact-row {
-			margin-top: 80px;
-		}
-
-		.form {
-			display: none;
-		}
-
-		.mobile-profile {
-			display: block;
-		}
-
 		.contacts {
+			width: 100%;
+
+			display: flex;
+			flex-direction: column;
+			justify-content: space-around;
+			align-items: center;
+			padding: 0 10vw;
 			margin-bottom: 60px;
+			margin-top: 100px;
+		}
+
+		.profile {
+			padding-bottom: 30px;
 		}
 	}
 </style>
